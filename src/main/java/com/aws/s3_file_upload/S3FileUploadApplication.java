6f7209dd8cdd3475f252b72ea1d23a37545cf919
srinarayan96 <shrinarayan1996@gmail.com>
2024-08-21 11:54:05 +0530

    initial setup

---
+++ src/main/java/com/aws/s3_file_upload/S3FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.aws.s3_file_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3FileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/s3_file_upload/S3FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.s3_file_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
